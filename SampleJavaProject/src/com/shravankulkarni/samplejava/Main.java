f0f184e2c84fc373514602c9bbb21b482cb7ba8f
Shravan Kulkarni <shravankulkarni786@gmail.com>
2017-05-24 19:43:57 +0530

    Uploading a sample project as facing issues in git uploading

---
+++ SampleJavaProject/src/com/shravankulkarni/samplejava/Main.java
@@ -0,0 +1,11 @@
+package com.shravankulkarni.samplejava;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
